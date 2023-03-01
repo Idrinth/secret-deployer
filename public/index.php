ed855f2a05a7f6caeaed89af6a481fd959b2a203
Björn Büttner <idrinth@users.noreply.github.com>
2023-03-01 09:59:32 +0100

    fix filename

--- public/index.php
+++ public/index.php
@@ -17,7 +17,7 @@ if (!isset($headers['X-GitHub-Event']) || $headers['X-GitHub-Event'] !== 'push')
     header('Content-Type: text/plain', true, 400);
     die();
 }
-foreach (Yaml::decodeFromFile(dirname(__DIR__) . '/config-yml') as $repository) {
+foreach (Yaml::decodeFromFile(dirname(__DIR__) . '/config.yml') as $repository) {
     if ($repository['project'] === $_POST['repository']['ssh_url']) {
         if ($repository['source'] !== $_SERVER['REMOTE_ADDR']) {
             header('Content-Type: text/plain', true, 403);
