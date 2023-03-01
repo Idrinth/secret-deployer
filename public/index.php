aa58fb3e4df4cb91425849c4ac0c0139dce91f20
Björn Büttner <idrinth@users.noreply.github.com>
2023-03-01 10:49:30 +0100

    manually writing git commands

--- public/index.php
+++ public/index.php
@@ -28,11 +28,9 @@ foreach (Yaml::decodeFromFile(dirname(__DIR__) . '/config.yml') as $repository) 
         $path = dirname(__DIR__) . '/cache/' . $post['repository']['full_name'];
         if (!is_dir($path)) {
             mkdir($path, 0700, true);
-            $repository = Admin::cloneRepository($path, $repository['project'], $key);
-            $repository->setDescription('Secrets cache');
+            exec('git clone -q -c core.sshCommand="/usr/bin/ssh -i ' . dirname(__DIR__) . '/private.key" ' . $repository['project']. ' ' . $path);
         } else {
-            $repository = new Repository($path);
-            $repository->run('pull', $key);
+            exec('cd ' . $path . '; git pull');
         }
         foreach ($repository['files'] as $file) {
             foreach (Glob::glob($path . '/' . $file['from']) as $f) {
