de0c0cedd13a92b228e98b4749fe66e73a0a5d38
Björn Büttner <idrinth@users.noreply.github.com>
2023-03-01 15:10:05 +0100

    working with cron instead

---
+++ bin/cron.php
@@ -0,0 +1,52 @@
+<?php
+
+use Symfony\Component\Yaml\Yaml;
+use Webmozart\Glob\Glob;
+
+$name = dirname(__DIR__) . '/cache/todo.' . microtime(true);
+rename(dirname(__DIR__) . '/cache/todo', $name);
+$moveables = [];
+foreach (Yaml::decodeFromFile(dirname(__DIR__) . '/config.yml') as $repository) {
+    $moveables[$repository['project']] = $repository['files'];
+}
+foreach (array_unique(explode("\n", file_get_contents($name))) as $line) {
+    if (!empty($line)) {
+        list($path, $source) = explode('|', $line);
+        $path = __DIR__ . '/../cache/' . $path;
+        if (!is_dir($path)) {
+            mkdir($path, 0700, true);
+            exec(
+                'git clone -c core.sshCommand="/usr/bin/ssh -i ' . dirname(__DIR__) . '/private.key" ' . $source. ' ' . $path,
+                $output,
+                $status
+            );
+            echo "Cloning to $path.";
+            foreach ($output as $out) {
+                echo "  $out";
+            }
+            echo "  Status $status";
+        } else {
+            exec(
+                'cd ' . $path . ' && git pull  -c core.sshCommand="/usr/bin/ssh -i ' . dirname(__DIR__) . '/private.key"',
+                $output,
+                $status
+            );
+            echo "Pulling to $path.";
+            foreach ($output as $out) {
+                echo "  $out";
+            }
+            echo "  Status $status";
+        }
+        foreach ($moveables[$source] as $glob) {
+            foreach (Glob::glob($path . '/' . $glob['from']) as $file) {
+                echo "  moving $file to {$glob['to-dir']}";
+                $dir = dirname($glob['to-dir'] . '/' . $glob['from']);
+                if (!is_dir($dir)) {
+                    mkdir($dir, 0777, true);
+                }
+                file_put_contents($glob['to-dir'] . '/' . $glob['from'], file_get_contents($file));
+            }
+        }
+    }
+}
+unlink($name);
--- public/index.php
+++ public/index.php
@@ -16,7 +16,6 @@ if (!isset($headers['X-GitHub-Event']) || $headers['X-GitHub-Event'] !== 'push')
     die();
 }
 $post = json_decode(file_get_contents('php://input'),true);
-$key = ['-c', 'core.sshCommand="/usr/bin/ssh -i ' . dirname(__DIR__) . '/private.key"'];
 foreach (Yaml::decodeFromFile(dirname(__DIR__) . '/config.yml') as $repository) {
     if ($repository['project'] === $post['repository']['ssh_url']) {
         if ($repository['source'] !== $_SERVER['REMOTE_ADDR']) {
@@ -24,49 +23,12 @@ foreach (Yaml::decodeFromFile(dirname(__DIR__) . '/config.yml') as $repository) 
             die();
         }
         $path = dirname(__DIR__) . '/cache/' . $post['repository']['full_name'];
-        $output = [];
-        if (!is_dir($path)) {
-            mkdir($path, 0700, true);
-            exec(
-                'git clone -c core.sshCommand="/usr/bin/ssh -i ' . dirname(__DIR__) . '/private.key" ' . $repository['project']. ' ' . $path,
-                $output,
-                $status
-            );
-            $output[] = "Cloning to $path.";
-        } else {
-            exec(
-                'cd ' . $path . ' && git pull  -c core.sshCommand="/usr/bin/ssh -i ' . dirname(__DIR__) . '/private.key"',
-                $output,
-                $status
-            );
-            $output[] = "Pulling to $path.";
-        }
-        if ($status !== 0) {
-            header('Content-Type: text/plain', true, 500);
-            $output[] = "Exited with $status.";
-            echo implode("\n", $output);
-            foreach ($output as $line) {
-                error_log($line);
-            }
-            die();
-        }
-        $status = 200;
-        foreach ($repository['files'] as $file) {
-            foreach (Glob::glob($path . '/' . $file['from']) as $f) {
-                $t = $file['to-dir'] . '/' . $file['from'];
-                if (copy($f, $t)) {
-                    $output[] = "copied $f to $t sucessfully";
-                } else {
-                    $output[] = "copying $f to $t failed";
-                    $status = 500;
-                }
-            }
-        }
-        header('Content-Type: text/plain', true, $status);
-        echo implode("\n", $output);
-        foreach ($output as $line) {
-            error_log($line);
-        }
+        file_put_contents(
+            dirname(__DIR__) . '/cache/todo',
+            "{$post['repository']['full_name']}|{$repository['source']}\n",
+            FILE_APPEND
+        );
+        header('Content-Type: text/plain', true, 200);
         die();
     }
 }
