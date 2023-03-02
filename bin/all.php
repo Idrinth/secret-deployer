7ee4b2f24222c8a66ba036e106e861598915792c
Björn Büttner <idrinth@users.noreply.github.com>
2023-03-02 16:13:58 +0100

    include autoloader and use statement

--- bin/all.php
+++ bin/all.php
@@ -1,5 +1,9 @@
 <?php
 
+use De\Idrinth\Yaml\Yaml;
+
+require_once (__DIR__ . '/../vendor/autoload.php');
+
 foreach (Yaml::decodeFromFile(dirname(__DIR__) . '/config.yml') as $repository) {
     $project = preg_replace('/^git@.*?:(.*)\.git$/', '$1', $repository['project']);
     file_put_contents(dirname(__DIR__) . '/cache/todo', "$path|{$repository['project']}\n", FILE_APPEND);
