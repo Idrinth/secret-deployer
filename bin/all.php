46abd1844cfeb8204a3747886e37ebb23ea99a91
Björn Büttner <idrinth@users.noreply.github.com>
2023-03-02 16:13:01 +0100

    add a fill todo script

---
+++ bin/all.php
@@ -0,0 +1,6 @@
+<?php
+
+foreach (Yaml::decodeFromFile(dirname(__DIR__) . '/config.yml') as $repository) {
+    $project = preg_replace('/^git@.*?:(.*)\.git$/', '$1', $repository['project']);
+    file_put_contents(dirname(__DIR__) . '/cache/todo', "$path|{$repository['project']}\n", FILE_APPEND);
+}
